1921ac8f605cbe6a569df70862d40606b7ab8274
icancclearynow <hello@iancleary.me>
2021-12-23 16:35:00 -0700

    Updated dev domain

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,16 +1,17 @@
-resource "vercel_domain" "icancclearynow_com" {
+# https://registry.terraform.io/providers/chronark/vercel/latest/docs
+resource "vercel_domain" "dev" {
   name = var.dev_domain
 }
 
 resource "vercel_dns" "www" {
-  domain = vercel_domain.icancclearynow_com.name
+  domain = vercel_domain.dev.name
   type   = "A"
   value  = "76.76.21.21"
   name   = "www"
 }
 
 resource "vercel_dns" "www2" {
-  domain = vercel_domain.icancclearynow_com.name
+  domain = vercel_domain.dev.name
   type   = "A"
   value  = "76.76.21.21"
   name   = "www2"
