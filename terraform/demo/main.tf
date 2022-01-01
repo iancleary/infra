34e472019fc2149c49f505163b1a0149ac83a7e6
icancclearynow <hello@iancleary.me>
2022-01-01 10:31:27 -0700

    Delete demo linode

--- terraform/demo/main.tf
+++ terraform/demo/main.tf
@@ -1,17 +1,1 @@
-module "node" {
 
-  source = "../modules/linode-instance"
-
-  label  = var.demo_nodename
-  image  = var.linode_image_ubu2004
-  region = var.linode_region_us_west
-  type   = var.linode_type_5usd_1gb
-  sshkeys = [
-    data.linode_sshkey.ian.ssh_key
-  ]
-  root_pass = yamldecode(file("~/.config/tokens/linode.yaml"))["default-root-pass"]
-}
-
-output "ipv4" {
-  value = tolist(module.node.ipv4)
-}
