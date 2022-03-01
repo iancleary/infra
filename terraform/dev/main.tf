227a5aeda4dcf491490869ee7a5b38e6d100296a
iancleary <hello@iancleary.me>
2022-02-28 20:16:48 -0700

    Ansible roles for mailserver testing and certbot install, terraform DNS

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -41,8 +41,13 @@ output "cloud1_ipv4" {
   value = tolist(linode_instance.cloud1.ipv4)[0]
 }
 
+output "cloud1_ipv6" {
+  value = replace(linode_instance.cloud1.ipv6, "/128", "")
+}
+
 locals {
   cloud1_ipv4 = tolist(linode_instance.cloud1.ipv4)[0]
+  cloud1_ipv6 = replace(linode_instance.cloud1.ipv6, "/128", "")
 }
 
 resource "vercel_domain" "dev" {
@@ -63,9 +68,168 @@ resource "vercel_dns" "www" {
   name   = "www"
 }
 
-resource "vercel_dns" "cloud1" {
+
+## --------------------------------------------
+## SPF1
+resource "vercel_dns" "spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = ""
+}
+
+resource "vercel_dns" "www_spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = "www"
+}
+
+resource "vercel_dns" "box_spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = "${var.nodename}"
+}
+
+resource "vercel_dns" "mta-sts_box_spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = "mta-sts.${var.nodename}"
+}
+
+resource "vercel_dns" "ns1_box_spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = "ns1.${var.nodename}"
+}
+
+resource "vercel_dns" "ns2_box_spf1" {
+  domain = vercel_domain.dev.name
+  type   = "TXT"
+  value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
+  name   = "ns2.${var.nodename}"
+}
+
+
+
+## --------------------------------------------
+## MX
+# https://github.com/chronark/terraform-provider-vercel/issues/51
+# 
+# resource "vercel_dns" "mx" {
+#   domain = vercel_domain.dev.name
+#   type   = "MX"
+#   value  = "mail.mailroute.net."
+#   name   = ""
+# }
+
+# resource "vercel_dns" "mx_box" {
+#   domain = vercel_domain.dev.name
+#   type   = "MX"
+#   value  = "mail.mailroute.net."
+#   name   = "${var.nodename}"
+# }
+
+## --------------------------------------------
+## A
+resource "vercel_dns" "autoconfig_ipv4" {
   domain = vercel_domain.dev.name
   type   = "A"
   value  = local.cloud1_ipv4
-  name   = "cloud1"
+  name   = "autoconfig"
+}
+
+resource "vercel_dns" "autodiscover_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "autodiscover"
+}
+
+resource "vercel_dns" "mta_sts_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "mta-sts"
+}
+
+resource "vercel_dns" "box_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "${var.nodename}"
+}
+
+resource "vercel_dns" "mta_sts_box_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "mta-sts.${var.nodename}"
+}
+
+resource "vercel_dns" "ns1_box_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "ns1.${var.nodename}"
+}
+
+resource "vercel_dns" "ns2_box_ipv4" {
+  domain = vercel_domain.dev.name
+  type   = "A"
+  value  = local.cloud1_ipv4
+  name   = "ns2.${var.nodename}"
+}
+
+## AAAA
+resource "vercel_dns" "autoconfig_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "autoconfig"
+}
+
+resource "vercel_dns" "autodiscover_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "autodiscover"
+}
+
+resource "vercel_dns" "mta_sts_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "mta-sts"
+}
+
+resource "vercel_dns" "box_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "${var.nodename}"
+}
+
+resource "vercel_dns" "mta_sts_box_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "mta-sts.${var.nodename}"
+}
+
+resource "vercel_dns" "ns1_box_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "ns1.${var.nodename}"
+}
+
+resource "vercel_dns" "ns2_box_ipv6" {
+  domain = vercel_domain.dev.name
+  type   = "AAAA"
+  value  = local.cloud1_ipv6
+  name   = "ns2.${var.nodename}"
 }
