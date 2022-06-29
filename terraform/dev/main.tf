114a1d27e0d982c27dbe676310ebd242cca3a1be
iancleary <hello@iancleary.me>
2022-06-28 18:48:16 -0700

    Migrate Vercel domain records from chronark/vercel to vercel/vercel (terraform providers)

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     vercel = {
-      source = "chronark/vercel"
-      version = "0.14.2"
+      source = "vercel/vercel"
+      version = "0.6.0"
     }
     linode = {
       source = "linode/linode"
@@ -15,7 +15,7 @@ terraform {
 
 provider "vercel" {
   # https://vercel.com/account/tokens
-  token = yamldecode(file("~/.config/tokens/vercel.yaml"))["token"]
+  api_token = yamldecode(file("~/.config/tokens/vercel.yaml"))["token"]
 }
 
 provider "linode" {
@@ -24,212 +24,34 @@ provider "linode" {
 }
 
 ###### END PROVIDER CONFIGURATION ######
-# resource "linode_instance" "cloud1" { 
 
-  
-#   image  = var.linode_image_ubu2004
-#   label  = var.nodename
-#   region = var.linode_region_us_west
-#   type   = var.linode_type_5usd_1gb
-#   authorized_keys = [
-#     data.linode_sshkey.ian.ssh_key
-#   ]
-#   root_pass = yamldecode(file("~/.config/tokens/linode.yaml"))["default-root-pass"]
-# }
-
-# output "cloud1_ipv4" {
-#   value = tolist(linode_instance.cloud1.ipv4)[0]
-# }
-
-# output "cloud1_ipv6" {
-#   value = replace(linode_instance.cloud1.ipv6, "/128", "")
-# }
-
-# locals {
-#   cloud1_ipv4 = tolist(linode_instance.cloud1.ipv4)[0]
-#   cloud1_ipv6 = replace(linode_instance.cloud1.ipv6, "/128", "")
-# }
-
-# resource "vercel_domain" "dev" {
-#   provider = vercel
-#   name = var.domain
-# }
-
-# resource "vercel_dns" "root" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = ""
-# }
-# resource "vercel_dns" "www" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "www"
-# }
+resource "vercel_project" "portfolio" {
+  name      = "dev"
+  framework = "nextjs"
 
 
-# ## --------------------------------------------
-# ## SPF1
-# resource "vercel_dns" "spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = ""
-# }
+  git_repository = {
+    type = "github"
+    repo = "iancleary/portfolio"
+  }
+}
 
-# resource "vercel_dns" "www_spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = "www"
-# }
+resource "vercel_project_domain" "portfolio" {
+  project_id = vercel_project.portfolio.id
+  domain     = var.domain
+}
 
-# resource "vercel_dns" "box_spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = "${var.nodename}"
-# }
+# A redirect of a domain name to a second domain name.
+# The status_code can optionally be controlled.
+resource "vercel_project_domain" "portfolio_redirect" {
+  project_id = vercel_project.portfolio.id
+  domain     = "www.${var.domain}"
 
-# resource "vercel_dns" "mta-sts_box_spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = "mta-sts.${var.nodename}"
-# }
+  redirect             = vercel_project_domain.portfolio.domain
+  redirect_status_code = 307
+}
 
-# resource "vercel_dns" "ns1_box_spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = "ns1.${var.nodename}"
-# }
-
-# resource "vercel_dns" "ns2_box_spf1" {
-#   domain = vercel_domain.dev.name
-#   type   = "TXT"
-#   value  = "v=spf1 mx ip4:${local.cloud1_ipv4} a:${vercel_domain.dev.name} a:${var.nodename}.${vercel_domain.dev.name} ~all"
-#   name   = "ns2.${var.nodename}"
-# }
-
-
-
-## --------------------------------------------
-## MX
-# https://github.com/chronark/terraform-provider-vercel/issues/51
-# 
-# resource "vercel_dns" "mx" {
-#   domain = vercel_domain.dev.name
-#   type   = "MX"
-#   value  = "mail.mailroute.net."
-#   name   = ""
-# }
-
-# resource "vercel_dns" "mx_box" {
-#   domain = vercel_domain.dev.name
-#   type   = "MX"
-#   value  = "mail.mailroute.net."
-#   name   = "${var.nodename}"
-# }
-
-## --------------------------------------------
-## A
-# resource "vercel_dns" "autoconfig_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "autoconfig"
-# }
-
-# resource "vercel_dns" "autodiscover_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "autodiscover"
-# }
-
-# resource "vercel_dns" "mta_sts_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "mta-sts"
-# }
-
-# resource "vercel_dns" "box_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "${var.nodename}"
-# }
-
-# resource "vercel_dns" "mta_sts_box_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "mta-sts.${var.nodename}"
-# }
-
-# resource "vercel_dns" "ns1_box_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "ns1.${var.nodename}"
-# }
-
-# resource "vercel_dns" "ns2_box_ipv4" {
-#   domain = vercel_domain.dev.name
-#   type   = "A"
-#   value  = local.cloud1_ipv4
-#   name   = "ns2.${var.nodename}"
-# }
-
-# ## AAAA
-# resource "vercel_dns" "autoconfig_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "autoconfig"
-# }
-
-# resource "vercel_dns" "autodiscover_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "autodiscover"
-# }
-
-# resource "vercel_dns" "mta_sts_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "mta-sts"
-# }
-
-# resource "vercel_dns" "box_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "${var.nodename}"
-# }
-
-# resource "vercel_dns" "mta_sts_box_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "mta-sts.${var.nodename}"
-# }
-
-# resource "vercel_dns" "ns1_box_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "ns1.${var.nodename}"
-# }
-
-# resource "vercel_dns" "ns2_box_ipv6" {
-#   domain = vercel_domain.dev.name
-#   type   = "AAAA"
-#   value  = local.cloud1_ipv6
-#   name   = "ns2.${var.nodename}"
-# }
+resource "vercel_deployment" "portfolio_example" {
+  project_id = vercel_project.portfolio.id
+  ref        = "main" # or a git branch
+}
--- terraform/prod/.terraform.lock.hcl
+++ terraform/prod/.terraform.lock.hcl
@@ -1,23 +1,25 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/chronark/vercel" {
-  version     = "0.14.2"
-  constraints = "0.14.2"
+provider "registry.terraform.io/vercel/vercel" {
+  version     = "0.6.0"
+  constraints = "0.6.0"
   hashes = [
-    "h1:WnFsoMGC3xb5q3dkmXinCeoosskjJxQn6Y/BVPoN9co=",
-    "zh:03f1ad08c0e81cecf586dbaa9f91ecab4ddaecd1f397c84ed72ddb5776baaa6c",
-    "zh:1e614e1837b7001abcb8e06ed05380bf920622b92f676cdde0932eea73909892",
-    "zh:292f9a537a31cf5461bd1042234b5b489368c3fbfd2978e1236946d7fe41e17a",
-    "zh:596b66ac7ab1c407db41aa5dd7c4541322b675bcf92aaec67c01488c4d81d9f8",
-    "zh:73d40adca8dd104274beb4cd678a938f7343f1bc3ebeb079297a9d81eea6ee72",
-    "zh:793205985ea5f5abe766a56e4247951c233746709541f6d997171cd0fde3eb06",
-    "zh:9e6d17295d8b761573d6b38c52551adf9956622ec470e7dc1823133e3edbcef7",
-    "zh:a109002abe3e632cfb93ddee09e60467179052ca4d66140b159a380d93853c54",
-    "zh:b20d3e2db26b5eac84eac139c9735786c220edd9f007ac108edf1b484b58ddeb",
-    "zh:c9b495f6223d2926efc1bf590bd00e4d6665296f14a9e7979b6e24c537c47bcc",
-    "zh:d8ed399ce021c5d1538cf58496c1c62ba4097e9bcff1fbb9ba1b675c98a373f7",
-    "zh:e06aa39761b17cccba3281122f75beec29b049546b98f6fdd9ddc8654e4d599f",
-    "zh:f789e502ff50febddc32ecc19e705e563d24a712433b3cff9c6b8832d7508b14",
+    "h1:KRQKZBZLA/M/zy/PAP+/rQlEwm1PEaU6+kRzCMtMkMw=",
+    "zh:08f359eae1b2371f51d988c7f94cb29eed43ed91f3151a02bf9bc980fc61d9ba",
+    "zh:2263b8b373f8edbc45df1112b6bc2d7dc1386110e6f442c193f5f8259eb0d60d",
+    "zh:27d8a54cd29b25e040480a5a88f9fed59e5011bd76e8c4420d6db3ce293f1936",
+    "zh:2a192d48b56259811e8af6bf59238d315e74fa0630895dbcfc1633198d0bfdc9",
+    "zh:3715a2a345e316241325fd9e77a4cd8bdaf34b40507fcca2037038f43626f383",
+    "zh:3df6b985782f86b3212a67ad779cf39618d44e34c213239f10e94ee19a022320",
+    "zh:583b25e9ac4693c3cb79422030b0f059fa38fe04185c29eaa30296e96c13396e",
+    "zh:6c0fc31b327303749b8ee271948f15cae4aac3d56be4dbcb93f9c09d7e60ec28",
+    "zh:81357d0dfbc81a96ef563244351f21942d314c3ef4a323bf81a0addcba0d6bc7",
+    "zh:a0b60135477d78a007190575408d3891d47fe115fa3feaa7dae388def082e62b",
+    "zh:b52d0e66d7606ce18566050e66b434af6276c93ccdf649ef51332d03aa66a7a6",
+    "zh:bbfd7ba05b5c67beaa3e1a525c5b9298fa8e68229d2755ce187016223389d08a",
+    "zh:c7971ca006f13284638aa84a8c569437f67008c54df9f2248addc138dd5a8b0a",
+    "zh:cd77e6a1e71c63a0ca138d8b9012a141ca42fb3571c3d6a6c3b6d8ccf6b04b4c",
+    "zh:f26e0763dbe6a6b2195c94b44696f2110f7f55433dc142839be16b9697fa5597",
   ]
 }
