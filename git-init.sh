28464263c287bee673434554c28b67973ee4192f
iancleary <hello@iancleary.me>
2022-09-06 17:07:43 -0700

    Update git-init for extra vault

--- git-init.sh
+++ git-init.sh
@@ -10,9 +10,17 @@ if [ -d .git/ ]; then
 rm .git/hooks/pre-commit
 cat << EOT >> .git/hooks/pre-commit
 if ( cat ansible/group_vars/raspberrypi3bv2p0/vault.yml | grep -q "\$ANSIBLE_VAULT;" ); then
-echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
+echo "[38;5;108mraspberrypi3bv2p0 Vault Encrypted. Safe to commit.[0m"
 else
-echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
+echo "[38;5;208mraspberrypi3bv2p0 Vault not encrypted! Run 'make encrypt' and try again.[0m"
+exit 1
+fi
+
+cat << EOT >> .git/hooks/pre-commit
+if ( cat ansible/group_vars/dns_local/vault.yml | grep -q "\$ANSIBLE_VAULT;" ); then
+echo "[38;5;108mdns_local Vault Encrypted. Safe to commit.[0m"
+else
+echo "[38;5;208mdns_local Vault not encrypted! Run 'make encrypt' and try again.[0m"
 exit 1
 fi
 EOT
