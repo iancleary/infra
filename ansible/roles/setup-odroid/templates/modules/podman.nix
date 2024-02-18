9c4ec7655461e8cbc4f5fb4c787721fc02eb46fd
Ian Cleary <github@iancleary.me>
2024-02-17 20:55:25 -0700

    Add ansible configuration of odroid nix files (#56)

---
+++ ansible/roles/setup-odroid/templates/modules/podman.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+  virtualisation.oci-containers.backend = "podman";
+
+  # Podman-compose
+  environment.systemPackages = [
+    pkgs.podman-compose
+  ];
+}
