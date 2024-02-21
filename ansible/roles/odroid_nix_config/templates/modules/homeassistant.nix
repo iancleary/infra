091b5df231891b39b3e103191f102d4ffbdc8f9d
Ian Cleary <github@iancleary.me>
2024-02-20 19:31:01 -0700

    Add TasmoAdmin Image to Docker Compose for Home Assistant (#77)

--- ansible/roles/odroid_nix_config/templates/modules/homeassistant.nix
+++ ansible/roles/odroid_nix_config/templates/modules/homeassistant.nix
@@ -4,7 +4,12 @@
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 1883 5353 8123 51827 ]; # MQTT, mDNS, Home Assistant Web UI, HomeKit
+      allowedTCPPorts = [ 1883 5353 8123 9541 51827 ];
+      # 1883 -> MQTT,
+      # 5353 -> mDNS,
+      # 8123 Home Assistant Web UI,
+      # 9541 -> TasmoAdmin,
+      # 51827 -> HomeKit
     };
   };
 }
