3bbfdac224deff0ba5a649f2335185eaa7283d39
iancleary <github@iancleary.me>
2024-03-11 10:53:48 -0700

    Work on odroid1 configuration and bringing services online

--- ansible/roles/odroid_nix_config/templates/modules/homeassistant.nix
+++ ansible/roles/odroid_nix_config/templates/modules/homeassistant.nix
@@ -4,11 +4,15 @@
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 1883 5353 8123 9541 51827 ];
+      allowedTCPPorts = [ 1883 5353 8123 8883 9001 9541 21064 21065 51827 ];
       # 1883 -> MQTT,
       # 5353 -> mDNS,
       # 8123 Home Assistant Web UI,
+      # 8883 -> MQTT over SSL,
+      # 9001 -> MQTT Websockets,
       # 9541 -> TasmoAdmin,
+      # 21064 -> Homekit Bridge
+      # 21065 -> Homekit Bridge (https://www.home-assistant.io/integrations/homekit/)
       # 51827 -> HomeKit
     };
   };
