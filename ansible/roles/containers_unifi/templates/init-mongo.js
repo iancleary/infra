77cb64b2401f235b266fcbca2b517420fe778d02
Ian Cleary <github@iancleary.me>
2024-10-28 12:46:44 -0700

    Remove unifi ansible role (#104)

--- ansible/roles/containers_unifi/templates/init-mongo.js
+++
@@ -1,2 +0,0 @@
-db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "{{ unifi_mongo_password }}", roles: [{role: "dbOwner", db: "unifi"}]});
-db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "{{ unifi_mongo_password }}", roles: [{role: "dbOwner", db: "unifi_stat"}]});
