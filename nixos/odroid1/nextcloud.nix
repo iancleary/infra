2b0c6875c4958a0908cf68c27f7b9f24f93cfdfb
iancleary <github@iancleary.me>
2025-11-30 19:48:22 -0700

    update nextcloud and nixpkgs

--- nixos/odroid1/nextcloud.nix
+++ nixos/odroid1/nextcloud.nix
@@ -17,7 +17,7 @@
       # hostName = "odroid1.tail2500d.ts.net";
       hostName = "nextcloud.iancleary.me";
       # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud32;
 
       # Storage path of nextcloud
       home = "/dpool/nextcloud";
