62a331a24ad7113154dbe89839c8aa349f5ae0df
Ian Cleary <github@iancleary.me>
2025-12-13 11:56:17 -0700

    Feat/nextcloud upgrade (#114)
    * Bump nextcloud major version
    
    * Bump nextcloud major version

--- nixos/odroid1/nextcloud.nix
+++ nixos/odroid1/nextcloud.nix
@@ -17,7 +17,7 @@
       # hostName = "odroid1.tail2500d.ts.net";
       hostName = "nextcloud.iancleary.me";
       # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud31;
+      package = pkgs.nextcloud32;
 
       settings.overwriteprotocol = "http";
 
